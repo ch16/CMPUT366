40fc8572146a5a333035e821151283a6fa557ad9
msabbasi <msabbasi@ualberta.ca>
2015-10-29 13:00:54 -0600

    Update randomPolicy.py

--- Project1/randomPolicy.py
+++ Project1/randomPolicy.py
@@ -1,12 +1,16 @@
 import blackjack
+import numpy as np
 from pylab import *
 
 numEpisodes = 2000
 
+def policy(s):
+    return np.random.randint(2)
+
 returnSum = 0.0
 for episodeNum in range(numEpisodes):
     G = 0
-    ...
+    
     print "Episode: ", episodeNum, "Return: ", G
     returnSum = returnSum + G
 print "Average return: ", returnSum/numEpisodes
