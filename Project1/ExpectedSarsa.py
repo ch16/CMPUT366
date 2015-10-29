cd2c3aafe09064370a87884c42c9a4646784a9d6
msabbasi <msabbasi@ualberta.ca>
2015-10-29 13:51:38 -0600

    Update ExpectedSarsa.py

--- Project1/ExpectedSarsa.py
+++ Project1/ExpectedSarsa.py
@@ -1,1 +1,25 @@
+import blackjack as bj
+import numpy as np
+from pylab import *
+
+numEpisodes = 2000
+returnSum = 0.0
+
+num_states = 180
+num_actions = 2
+
+alpha = 0.001
+
+Q =  0.00001*rand(num_states,num_actions)
+
+for episodeNum in range(numEpisodes):
+    G = 0
+    s = bj.init()
+    while s != -1:
+        r, s = bj.sample(s,np.random.randint(2))
+        G+=r
+    
+    print "Episode: ", episodeNum, "Return: ", G
+    returnSum = returnSum + G
+print "Average return: ", returnSum/numEpisodes
 
