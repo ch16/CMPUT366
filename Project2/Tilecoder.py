f8c64912de9ca88ac57ced94dff3b249773e4712
msabbasi <msabbasi@ualberta.ca>
2015-11-16 18:05:40 -0700

    fixed a bug

--- Project2/Tilecoder.py
+++ Project2/Tilecoder.py
@@ -5,9 +5,9 @@ numTilings = 8
 def tilecode(x,y,tileIndices):
 	# write your tilecoder here (5 lines or so)
 	for i in range(numTilings):
-		xoffset = math.floor((x+i*0.6/numTilings)/10)
-		yoffset = math.floor((y+i*0.6/numTilings)/10)
-		tileIndices[i] = i*121+xoffset+10*yoffset
+		xoffset = math.floor((x+i*0.6/numTilings)*10/6)
+		yoffset = math.floor((y+i*0.6/numTilings)*10/6)
+		tileIndices[i] = i*121+xoffset+11*yoffset
     
 def printTileCoderIndices(x,y):
 	tileIndices = [-1]*numTilings
