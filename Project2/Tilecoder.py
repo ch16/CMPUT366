3e208d605863592e37148d95b95c10e71c143122
msabbasi <msabbasi@ualberta.ca>
2015-11-16 18:23:28 -0700

    final verion I think

--- Project2/Tilecoder.py
+++ Project2/Tilecoder.py
@@ -1,13 +1,11 @@
-from pylab import math
-
 numTilings = 8
     
 def tilecode(x,y,tileIndices):
 	# write your tilecoder here (5 lines or so)
 	for i in range(numTilings):
-		xoffset = math.floor((x+i*0.6/numTilings)*10/6)
-		yoffset = math.floor((y+i*0.6/numTilings)*10/6)
-		tileIndices[i] = i*121+xoffset+11*yoffset
+		xindex = int((x+i*0.6/numTilings)*10/6)
+		yindex = int((y+i*0.6/numTilings)*10/6)
+		tileIndices[i] = i*121+xindex+11*yindex
     
 def printTileCoderIndices(x,y):
 	tileIndices = [-1]*numTilings
