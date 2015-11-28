dc5703a7e9e2a529d97c7a808b8097a348a60bdb
msabbasi <msabbasi@ualberta.ca>
2015-11-27 19:32:46 -0700

    Added the not working algorithm

--- Project3/learning.py
+++ Project3/learning.py
@@ -1,26 +1,52 @@
-import mountaincar
+import mountaincar as mc
+import numpy
 from Tilecoder import numTilings, tilecode, numTiles
 from Tilecoder import numTiles as n
 from pylab import *  #includes numpy
 
 numRuns = 1
 numEpisodes = 200
-alpha = 005/numTilings
+numActions = 3
+alpha = 0.5/numTilings
 gamma = 1
 lmbda = 0.9
 Epi = Emu = epsilon = 0
-n = numTiles * 3
+n = numTilings * numTiles * numActions
 F = [-1]*numTilings
 
 runSum = 0.0
 for run in xrange(numRuns):
     theta = -0.01*rand(n)
+    Q=numpy.zeros(numActions)
     returnSum = 0.0
     for episodeNum in xrange(numEpisodes):
         G = 0
-        ...
-        your code goes here (20-30 lines, depending on modularity)
-        ...
+        e=[0]*n
+        #your code goes here (20-30 lines, depending on modularity)
+        s = mc.init()
+        tilecode(s[0],s[1],F)
+        for a in range(3):
+	    for i in F:
+	        Q[a]+= theta[i+a*324]
+        a = numpy.argmax(Q)
+        r, s1 = mc.sample(s,a)
+        delta=r-Q[a]
+        for i in F:
+    	    e[i]=1
+        if s1 == None:
+	    for i in range(n):
+	        theta[i]=theta[i]+alpha*delta*e[i]
+	    break
+        for a in range(3):
+	    for i in F:
+	        Q[a]+= theta[i+a*324]
+        delta=delta+numpy.max(Q)
+        for i in range(n):
+	    theta[i]=theta[i]+alpha*delta*e[i]
+            e[i]=lmbda*e[i]      
+        s = s1
+        
+
         print "Episode: ", episodeNum, "Steps:", step, "Return: ", G
         returnSum = returnSum + G
     print "Average return:", returnSum/numEpisodes
