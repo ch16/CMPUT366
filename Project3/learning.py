5786070b40f34067bb4ba108f7e93179eb3450af
msabbasi <msabbasi@ualberta.ca>
2015-11-29 15:02:22 -0700

    Functional code

--- Project3/learning.py
+++ Project3/learning.py
@@ -5,7 +5,7 @@ from Tilecoder import numTiles as n
 from pylab import *  #includes numpy
 
 numRuns = 1
-numEpisodes = 200
+numEpisodes = 20
 numActions = 3
 alpha = 0.5/numTilings
 gamma = 1
@@ -14,14 +14,21 @@ Epi = Emu = epsilon = 0
 n = numTilings * numTiles * numActions
 F = [-1]*numpy.ones(numTilings)
 
+def Qs(F):
+    Q=numpy.zeros(numActions)
+    for a in range(3):
+        for i in F:
+            Q[a]+= theta[i+a*324]
+    return Q
+
 runSum = 0.0
 for run in xrange(numRuns):
-    theta = -0.001*numpy.random.rand(n)
+    theta = -0.01*numpy.random.rand(n)
     returnSum = 0.0
     for episodeNum in xrange(numEpisodes):
         G = 0
-        step=0
         #your code goes here (20-30 lines, depending on modularity)
+        step=0
         e=numpy.zeros(n)
         s = mc.init()
         Q=numpy.zeros(numActions)
@@ -29,10 +36,7 @@ for run in xrange(numRuns):
             #print Q
             step+=1
             tilecode(s[0],s[1],F)
-            Q=numpy.zeros(numActions)
-            for a in range(3):
-                for i in F:
-                    Q[a]+= theta[i+a*324]
+            Q=Qs(F)
             a = numpy.argmax(Q)
             r, s1 = mc.sample(s,a)
             G+=r
@@ -43,26 +47,25 @@ for run in xrange(numRuns):
                 for i in range(n):
                     theta[i]+=alpha*delta*e[i]
                 break
-            tilecode(s[0],s[1],F)
-            Q=numpy.zeros(numActions)
-            for a in range(3):
-                for i in F:
-                    Q[a]+= theta[i+a*324]
+            tilecode(s1[0],s1[1],F)
+            Q=Qs(F)
             delta=delta+numpy.max(Q)
             for i in range(n):
                 theta[i]+=alpha*delta*e[i]
                 e[i]=lmbda*e[i]
             s = s1
         
-    
         print "Episode: ", episodeNum, "Steps:", step, "Return: ", G
         returnSum = returnSum + G
     print "Average return:", returnSum/numEpisodes
     runSum += returnSum
 print "Overall performance: Average sum of return per run:", runSum/numRuns
 
+
 #Additional code here to write average performance data to files for plotting...
 #You will first need to add an array in which to collect the data
+
+
 
 def writeF():
     fout = open('value', 'w')
@@ -76,4 +79,4 @@ def writeF():
         fout.write('\n')
     fout.close()
 
-
+writeF()
